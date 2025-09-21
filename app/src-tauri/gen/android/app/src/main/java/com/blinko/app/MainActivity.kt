708ab550967cb34f93b537d54126a8af52d832de
Blinko <blinkospace@gmail.com>
2025-09-21 08:52:20 +0800

    🐛fix: remove forced white status bar in Android onResume
    Remove setWhiteSystemBars() call from MainActivity.onResume() to prevent
    status bar color flashing between white and theme color when switching
    between light/dark modes or returning from background.

--- app/src-tauri/gen/android/app/src/main/java/com/blinko/app/MainActivity.kt
+++ app/src-tauri/gen/android/app/src/main/java/com/blinko/app/MainActivity.kt
@@ -170,44 +170,4 @@ class MainActivity : TauriActivity() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        setWhiteSystemBars()
-    }
-    
-    private fun setWhiteSystemBars() {
-        try {
-            val whiteColor = Color.WHITE
-            
-            window.statusBarColor = whiteColor
-            window.navigationBarColor = whiteColor
-            
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                window.decorView.post {
-                    window.insetsController?.let { controller ->
-                        controller.setSystemBarsAppearance(
-                            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
-                            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
-                        )
-                        controller.setSystemBarsAppearance(
-                            WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
-                            WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
-                        )
-                    }
-                }
-            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                // Android 6.0 - 10
-                var flags = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-                
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    flags = flags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-                }
-                
-                @Suppress("DEPRECATION")
-                window.decorView.systemUiVisibility = flags
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
 }
